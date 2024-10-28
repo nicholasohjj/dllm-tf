80b641837980d4a96e6e9f65aae532de37dea8e8
Nicholas Oh <nicholasohjj@gmail.com>
2024-10-28 21:28:43 +0800

    added remote backend

--- main.tf
+++ main.tf
@@ -1,3 +1,12 @@
 provider "aws" {
   region = var.aws_region
 }
+
+terraform {
+  backend "s3" {
+    bucket         = "terraform-bucket-dllm"  # Replace with your bucket name
+    key            = "dllm-tf/terraform.tfstate"    # Set a unique path for your project
+    region         = "ap-southeast-1"                    # Replace with your region
+    encrypt        = true
+  }
+}
--- s3.tf
+++ s3.tf
@@ -5,3 +5,14 @@ resource "aws_s3_bucket" "archived-data" {
 resource "aws_s3_bucket" "pretrained-model" {
   bucket = "pretrained-model-dllm"
 }
+
+resource "aws_s3_bucket" "terraform-bucket" {
+  bucket = "terraform-bucket-dllm"
+}
+
+resource "aws_s3_bucket_versioning" "terraform-bucket-versioning" {
+  bucket = aws_s3_bucket.terraform-bucket.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
